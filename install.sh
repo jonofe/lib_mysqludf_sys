05fef5906f754e812d43d09debf1f13a9f8faf13
jonofe <andre.feld@anrath.net>
2017-05-07 15:29:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ fi
 
 echo -e "\nPlease provide your MySQL root password"
 
-mysql -u root -p mysql < lib_mysqludf_sys.sql
+mysql -u root mysql < lib_mysqludf_sys.sql
 
 if test $? -ne 0; then
 	echo "ERROR: unable to install the UDF"
